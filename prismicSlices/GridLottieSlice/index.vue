557a5f11afaecba007f9efff39de5f6da99c2eb2
Vladislav Kochkin <vladislav.k@maddevs.io>
2024-02-10 23:08:08 +0600

    MAR-6234. Add GridLottieSlice.

---
+++ prismicSlices/GridLottieSlice/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { slice } = defineProps(getSliceComponentProps(['slice', 'index', 'slices', 'context']))
+</script>
+<template>
+  <LazyGridLottieLink
+    v-if="slice.variation === 'gridLottieLink'"
+    :slice="slice"
+  />
+  <LazyGridLottieWithCaseStudy
+    v-if="slice.variation === 'gridLottieWithCaseStudy'"
+    :slice="slice"
+  />
+  <LazyGridLottieWithAnimationOptions
+    v-if="slice.variation === 'gridWithAnimationOptionSlice'"
+    :slice="slice"
+  />
+</template>
