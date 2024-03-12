739209704dd88c9c69dafc3777849037ff71d52d
OlgaLookina <olga.l@maddevs.io>
2024-03-12 14:50:04 +0300

    MAR-6191. Add transformation method.

--- components/Writeup/helpers/transformationWriteupListData.ts
+++ components/Writeup/helpers/transformationWriteupListData.ts
@@ -1,4 +1,5 @@
 import type { TransformedWriteup, Writeups } from '~/components/Writeup/interfaces/IWriteupList'
+import { transformationWriteupItems } from '~/components/Writeup/helpers/transformationWriteupItems'
 
 interface TransformedData {
   totalPages: number
@@ -8,20 +9,7 @@ interface TransformedData {
 }
 
 export const transformationWriteupListData = (writeups: Writeups): TransformedData => {
-  const writeupList = writeups.results && writeups.results.length
-    ? writeups.results.map(writeup => ({
-      uid: writeup.uid,
-      date: formatDate(writeup.data.created_date, true),
-      title: writeup.data.title,
-      tags: writeup.tags.filter(tag => tag !== 'Writeup'),
-      author: {
-        name: writeup?.data?.author?.data?.name,
-        position: writeup?.data?.author?.data?.position,
-        uid: writeup?.data?.author?.data?.uid,
-        image: writeup?.data?.author?.data?.image,
-      },
-    }))
-    : []
+  const writeupList = transformationWriteupItems(writeups.results)
 
   return {
     totalPages: writeups?.total_pages,
