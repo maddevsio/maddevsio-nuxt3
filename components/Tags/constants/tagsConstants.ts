119fbd010f004f7f4b1d949e1840a67616b42709
Vladislav Kochkin <vladislav.k@maddevs.io>
2024-03-12 14:45:34 +0600

    MAR-6200. Add tag pages.

---
+++ components/Tags/constants/tagsConstants.ts
@@ -0,0 +1,82 @@
+export const tagsDescriptions = [
+  {
+    title: 'Project Management',
+    description: 'Project managers need to continuously improve their skills and knowledge to keep up with the changes in their organizations. Browse our articles to learn about various project management techniques and methodologies.',
+  },
+  {
+    title: 'Software Development',
+    description: 'Mad Devs software development articles provide information about technologies or practical solutions that help you complete a project and improve your business performance.',
+  },
+  {
+    title: 'Mobile Development',
+    description: 'Many businesses are generating revenue through smartphone applications. Check our mobile development series of articles to choose the right approach and tools for developing Android or IOS mobile app.',
+  },
+  {
+    title: 'DevOps',
+    description: 'DevOps represents the merging of software development and IT operations into a single discipline, so it seems vague. Read our insightful collection of articles to better understand DevOps practices.',
+  },
+  {
+    title: 'Hardware',
+    description: 'Due to its complexity, computer hardware always involves looking for physical defects and performing various tests to see if they can affect the system\'s performance. Discover tech articles on hardware and computer science. ',
+  },
+  {
+    title: 'Frontend development',
+    description: 'Mad Devs articles cover topics about frontend development, including accessibility, usability, and design patterns for the website\'s interface and mobile devices.',
+  },
+  {
+    title: 'Backend development',
+    description: 'Backend developers are responsible for maintaining and improving the security of their data. They focus on everything you can\'t see on a website. Discover articles covering APIs, server-side scripting, middleware, the cloud, databases, servers, and more.',
+  },
+  {
+    title: 'Python',
+    description: 'Over the past two decades, the popularity of Python has increased significantly. Mad Devs Python-related practical articles are constantly updated with the latest news and techniques that can be used to improve your skills.',
+  },
+  {
+    title: 'Jira',
+    description: 'Jira is specifically designed to help teams of all types manage work. Check Mad Devs articles about management with Jira to improve processes and workflow and help your team and customers work and communicate effectively.',
+  },
+  {
+    title: 'Mad Culture',
+    description: 'The corporate culture supports business objectives and directly impacts the company’s overall health, mood, and attitudes of employees and customers. Read our collection of articles where we share our knowledge and experience about Mad Culture.',
+  },
+  {
+    title: 'Mad Vision',
+    description: 'At Mad Devs, we adhere to the principle that knowledge and expertise should be shared. In this series of articles, you will find our employees\' vision, opinions, and thoughts on many topics and trends in the IT industry.',
+  },
+  {
+    title: 'Blockchain',
+    description: 'Blockchain is a shared, immutable ledger that enables the synchronization and recording of transactions within a business network. Read our articles that cover the basics and blockchain-related new technologies.',
+  },
+  {
+    title: 'FinTech',
+    description: 'FinTech refers to the integration of various technologies into the offerings of financial services companies. Check Mad Devs\' practical and knowledgeable articles about multiple innovations, technologies, and projects to improve financial services\' efficiency and effectiveness.',
+  },
+  {
+    title: 'Quality Assurance',
+    description: 'With Quality Assurance, you get a process that involves identifying and improving the quality of a company\'s products and services. Check our series of articles focused on various insights and testing to ensure they are of the best quality.',
+  },
+  {
+    title: 'Design',
+    description: 'Here are a series of Mad Devs articles about the web, mobile, graphic, UI/UX, accessible design, and more. Find your inspiration and deep knowledge tutorials.',
+  },
+  {
+    title: 'HealthTech',
+    description: 'HealthTech refers to a broad range of technologies or offerings to improve healthcare quality or consumer care. Read Mad Devs articles about innovations enabled by engineering or modern computing in industry.',
+  },
+  {
+    title: 'Transportation',
+    description: 'New technologies influence the availability of transport and the various trade opportunities. Browse our series of articles with practical solutions  to improve the efficiency of the transport industry.',
+  },
+  {
+    title: 'Editors pick',
+    description: 'In this section, you can read the best and most popular Mad Devs articles that our experts prepared on a variety of topics in the field of IT.',
+  },
+  {
+    title: 'Cybersecurity',
+    description: 'Cybersecurity is the practice of protecting critical systems and sensitive information from digital attacks. Browse our articles to learn about best practices for securing digital assets, interviews with experts, and reviews of security products and services.',
+  },
+  {
+    title: 'Machine Learning',
+    description: 'ML is a type of artificial intelligence (AI) that allows software applications to become more accurate at predicting outcomes. Read our articles about popular ML algorithms and techniques and practical ML applications in various finance, healthcare, and marketing domains.',
+  },
+]
