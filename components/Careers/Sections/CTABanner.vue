0f6e7d521bac94ff0ce795b5e3fb017b2162e2c3
OlgaLookina <olga.l@maddevs.io>
2024-04-05 12:39:46 +0300

    MAR-6500. Replace image.

--- components/Careers/Sections/CTABanner.vue
+++ components/Careers/Sections/CTABanner.vue
@@ -16,7 +16,7 @@ const { $getMediaFromS3 } = useMediaFromS3()
     <LazySharedUIBanner
       title="Can’t see your next role?"
       text="If you don’t see a suitable vacancy, but you are sure we're meant to be together, drop us a line."
-      :image="$getMediaFromS3(`/images/CTABanner/ekaterina-bludova.webp`)"
+      :image="$getMediaFromS3(`/images/CTABanner/ekaterina-bludova2x.webp`)"
       image-alt-text="Ekaterina Bludova"
       man-name="Ekaterina Bludova"
       man-position="Recruitment Team Leader"
