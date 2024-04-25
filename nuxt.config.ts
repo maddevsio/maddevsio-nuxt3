c43e4b0890211b8f39ec5f248928588efba282f3
Vladislav Kochkin <vladislav.k@maddevs.io>
2024-04-25 17:00:48 +0600

    MAR-6522. Return mount for nuxt-delay-hydration.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -99,7 +99,7 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@nuxtjs/device',
     ['nuxt-delay-hydration', {
-      mode: 'init',
+      mode: 'mount',
       debug: process.env.NODE_ENV === 'development',
     }],
     ['nuxt-swiper', {
