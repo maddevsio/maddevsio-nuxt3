2d63df31019a3f48f724cd51c8fff5cf3a2719db
OlgaLookina <olga.l@maddevs.io>
2024-06-20 12:34:10 +0300

    MAR-6107. Fix check query function.

--- utils/checkParametersForQuery.ts
+++ utils/checkParametersForQuery.ts
@@ -1,13 +1,10 @@
 export const checkParametersForQuery = (pageName: string, mainTag: string, routeQuery: Record<string, string>) => {
   const firstPage = 1
-  const checkedTag = checkTagCloudName(routeQuery.tag)
-  if (pageName in routeQuery && !('tag' in routeQuery)) {
-    return { tags: [mainTag], page: Number(routeQuery[pageName]) };
-  } else if (pageName in routeQuery && 'tag' in routeQuery) {
-    return { tags: [checkedTag], page: Number(routeQuery[pageName]) };
-  } else if ('tag' in routeQuery && !(pageName in routeQuery)) {
-    return { tags: [checkedTag], page: firstPage };
-  } else {
-    return { tags: [mainTag], page: firstPage };
-  }
-};
+  const { tag, [pageName]: page } = routeQuery
+  const checkedTag = checkTagCloudName(tag)
+
+  const tags = tag ? [checkedTag] : [mainTag]
+  const pageNum = page ? Number(page) : firstPage
+
+  return { tags, page: pageNum }
+}
