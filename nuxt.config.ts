e83279740facecdf77a3578a0cbfeea036f88742
Vladislav Kochkin <vladislav.k@maddevs.io>
2024-08-09 12:26:30 +0600

    MAR-6756. Enhance cookie consent management.
    Updated the cookie consent mechanism to include granular control for different types of user data storage. Modified the UI to provide users with options for managing their preferences, and updated the Google Tag Manager script to reflect consent preferences dynamically.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,7 +74,7 @@ export default defineNuxtConfig({
           body: true,
           vmid: 'gtag-script',
 
-          innerHTML: `window.dataLayer = window.dataLayer || []; function gtag() {dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config', '${ process.env.NODE_GA4_KEY }', {send_page_view: false,});`,
+          innerHTML: `window.dataLayer = window.dataLayer || []; function gtag() {dataLayer.push(arguments)}; gtag('consent', 'default', {'ad_storage': 'granted','ad_user_data': 'granted','ad_personalization': 'granted','analytics_storage': 'granted'}); gtag('js', new Date()); gtag('config', '${ process.env.NODE_GA4_KEY }', {send_page_view: false,});`,
         },
         process.env.FF_ENVIRONMENT === 'production' && process.env.LINKEDIN_SCRIPT_TURN_ON === 'on'
           ? {
